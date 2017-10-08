90ac90e91f6cd17edaf0769510ec88b0d0f581ef
Phil Kunz <gitlab@philkunz.com>
2017-10-08 15:25:49 +0200

    fix path to assets dir

--- ts/vscts.paths.ts
+++ ts/vscts.paths.ts
@@ -5,7 +5,7 @@ export let cwd = process.cwd()
 export let dotVscodeDir = plugins.path.join(cwd, '.vscode/')
 
 export let packageDir = plugins.path.join(__dirname, '../')
-export let assetDir = plugins.path.join(packageDir, '../assets/')
+export let assetDir = plugins.path.join(packageDir, './assets/')
 
 // files
 export let launchJson = plugins.path.join(dotVscodeDir, 'launch.json')
